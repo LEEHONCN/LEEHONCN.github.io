ab35cbfbf0c436a1470a44f4d98eb8c6bd2340b0
lihang <lihang@growingio.com>
2022-01-08 15:06:04 +0800

    add

--- init.js
+++ init.js
@@ -1,6 +1,6 @@
 if ('serviceWorker' in navigator) {
 	window.addEventListener('load', function () {
-			navigator.serviceWorker.register('/service-worker.js').then(function (registration) {
+			navigator.serviceWorker.register('/service-worker.js', {scope: '.'}).then(function (registration) {
 					console.log('我注册成功了666');
 			}, function (err) {
 					console.log('我注册失败了');
--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,15 @@
 console.log('加载了sw')
-self.addEventListener('install', function (event) {
-	console.log('安装')
+self.addEventListener('fetch', function(event) {
+	console.log('fetch')
+	event.respondWith(async function() {
+		 try{
+			 var res = await fetch(event.request);
+			 var cache = await caches.open('cache');
+			 cache.put(event.request.url, res.clone());
+			 return res;
+		 }
+		 catch(error){
+			 return caches.match(event.request);
+			}
+		}());
 });
